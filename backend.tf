8a4224429f422ab48af11d55a3a40075aff6ce4c
Gvrkprasad <glpsk369@gmail.com>
2025-08-15 11:30:16 +0530

    terraform files added

---
+++ backend.tf
@@ -0,0 +1,22 @@
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "terraform-lock-ansible"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "backend-bucket-ansible"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "tf-state-lock-ansible"
+  }
+}
+
+
