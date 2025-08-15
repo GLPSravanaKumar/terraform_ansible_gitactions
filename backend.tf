7317003f218eb8679cf30b0d0db9dbd7907e1751
Gvrkprasad <glpsk369@gmail.com>
2025-08-15 22:30:36 +0530

    backend updated

--- backend.tf
+++ backend.tf
@@ -9,13 +9,13 @@ resource "aws_dynamodb_table" "tf_lock" {
   }
 }
 
+
 terraform {
   backend "s3" {
-    bucket         = "backend-bucket-ansible"
-    key            = "terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
-    dynamodb_table = "tf-lock-ansible"
+    bucket  = "backend-bucket-ansible"
+    key     = "terraform.tfstate"
+    region  = "ap-south-1"
+    encrypt = true
   }
 }
 
