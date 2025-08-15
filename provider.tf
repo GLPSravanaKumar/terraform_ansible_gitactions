1d1814298e618fb7a6540a7ffa1f037e0ba5405a
Gvrkprasad <glpsk369@gmail.com>
2025-08-15 22:44:43 +0530

    secret resource deleted

--- provider.tf
+++ provider.tf
@@ -8,19 +8,6 @@ terraform {
 }
 
 provider "aws" {
-  region     = "ap-south-1"
-  access_key = local.aws_creds.AWS_ACCESS_KEY_ID
-  secret_key = local.aws_creds.AWS_SECRET_ACCESS_KEY
+  region = "ap-south-1"
 }
 
-data "aws_secretsmanager_secret" "secret" {
-  name = "optum_access_key"
-}
-
-data "aws_secretsmanager_secret_version" "secret_value" {
-  secret_id = data.aws_secretsmanager_secret.secret.id
-}
-
-locals {
-  aws_creds = jsondecode(data.aws_secretsmanager_secret_version.secret_value.secret_string)
-}
