c283dd7820e1f98d34e8a63c129f8327d0211dfd
Gvrkprasad <glpsk369@gmail.com>
2025-08-17 12:32:04 +0530

    datasource mandoDb added

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "mongodb" {
-  source            = "./modules/mongodb"
+  source            = "git::https://github.com/GLPSravanaKumar/terraform_ansible_gitactions.git//modules/mongodb"
   atlas_public_key  = var.atlas_public_key
   atlas_private_key = var.atlas_private_key
   project_id        = var.project_id
