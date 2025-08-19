f803e21b17c7d195daf3de8e1e1b6a09bc6f466f
Gvrkprasad <glpsk369@gmail.com>
2025-08-19 18:57:13 +0530

    updated files

---
+++ modules/aws_s3_bucket/main.tf
@@ -0,0 +1,59 @@
+resource "aws_s3_bucket" "example" {
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.env
+  }
+}
+
+
+resource "aws_s3_bucket_policy" "policy_attachment" {
+  bucket = aws_s3_bucket.example.id
+  policy = data.aws_iam_policy_document.bucket_policy.json
+
+}
+
+data "aws_iam_policy_document" "bucket_policy" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["593793035673"]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.example.arn,
+      "${aws_s3_bucket.example.arn}/*",
+    ]
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.example.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [aws_s3_bucket_ownership_controls.example]
+
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_object" "object" {
+  bucket = aws_s3_bucket.example.id
+  key    = "ganesh.jpeg"
+  source = "C:/glps/terraform_project/terraform_ansible/ganesh.jpeg"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  content_type = "text/html"
+  acl          = "public-read"
+  etag         = filemd5("C:/glps/terraform_project/terraform_ansible/ganesh.jpeg")
+}
