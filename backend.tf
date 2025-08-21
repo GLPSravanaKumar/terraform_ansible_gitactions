0a7b412e6354c473401ed1704467cb84600f2a74
Gvrkprasad <glpsk369@gmail.com>
2025-08-21 22:05:23 +0530

    profile optum commnted in backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = "backend-bucket-ansible"
-    key     = "terraform.tfstate"
-    region  = "ap-south-1"
-    profile = "optum"
+    bucket = "backend-bucket-ansible"
+    key    = "terraform.tfstate"
+    region = "ap-south-1"
+    #  profile = "optum"
     encrypt = true
     #  dynamodb_table = "tf-lock-ansible"
   }
