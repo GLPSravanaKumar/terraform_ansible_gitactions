0bae5f97f0d32a36599cde53440cb1c5426e6000
Gvrkprasad <glpsk369@gmail.com>
2025-08-24 20:25:18 +0530

    tf backend s3 region changed to us-east-1

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "backend-bucket-ansible"
     key    = "terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
     #  profile = "optum"
     encrypt        = true
     dynamodb_table = "glps_backend_lock_table"
