4d418b175dadc113e041c290c89a170df36972bb
Gvrkprasad <glpsk369@gmail.com>
2025-08-25 07:50:18 +0530

    vpc_security_group_ids filed modified in aws_instanc resource

--- modules/aws_ec2/main.tf
+++ modules/aws_ec2/main.tf
@@ -23,7 +23,7 @@ resource "aws_instance" "public_servers" {
   key_name                    = data.aws_key_pair.key.key_name
   subnet_id                   = element(var.public_subnet_ids, count.index)
   associate_public_ip_address = true
-  vpc_security_group_ids      = ["var.pub_sg_id"]
+  vpc_security_group_ids      = [var.pub_sg_id]
   user_data                   = <<-EOF
         #!/bin/bash
         sudo apt update -y
