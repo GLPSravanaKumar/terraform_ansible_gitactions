10c5307fabc4e2989ee649f29d046386ae7a4f65
Gvrkprasad <glpsk369@gmail.com>
2025-08-25 07:27:34 +0530

    sample userdata added in module.aws_ec2

--- modules/aws_ec2/main.tf
+++ modules/aws_ec2/main.tf
@@ -24,6 +24,16 @@ resource "aws_instance" "public_servers" {
   subnet_id                   = element(var.public_subnet_ids, count.index)
   associate_public_ip_address = true
   vpc_security_group_ids      = ["var.pub_sg_id"]
+  user_data                   = <<-EOF
+        #!/bin/bash
+        sudo apt update -y
+        sudo apt install -y httpd
+        sudo systemctl start httpd
+        sudo systemctl enable httpd
+        echo "<h1>Hello from Terraform!</h1>" > /var/www/html/index.html
+      EOF
+
+
   tags = {
     Name = "public_webserver-${count.index + 1}"
   }
